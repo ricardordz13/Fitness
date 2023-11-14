d7a756f2efd5f3bee794a7e0126f6ca70a16b5d3
ricardordz13 <ricardo.rdz.trevino@gmail.com>
2023-11-14 12:18:40 +0100

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Ricardo Jorge Rodriguez Trevino on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
